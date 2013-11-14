f2133f8416b6c17449c194dd01514d59b89eef70
jo5ch <josch243@gmail.com>
2013-11-14 02:14:26 -0800

    Documentation for Simulation.h added

--- include/Simulation.h
+++ include/Simulation.h
@@ -6,15 +6,34 @@
 #include "TechnologyManager.h"
 #include "Entity.h"
 
+/** Main class designed for managing and controlling the Simulation.
+ * 
+ * During each timestep the Simulation decided which actions to execute and 
+ * requests the TechnologyManager if the requirements for this action are fulfilled. 
+ * If this applies, the actions are executed by changing the GameState object and the Entities that are part of it.
+ * If not the Simulation decides itself what to do else.
+ * After the Simulation is done with all activities for the current timestep, it procedes to the next one by calling timeStep().
+ * The Simulation itself is started and executed by calling the startSimulation() function,
+ * which creates all required objects before entering the first timestep.
+ *
+ */
+
 class Simulation
 {
 	private:
-		GameState * gameState;
-		ResourceManager * resourceManager;
-		TechnologyManager * technologyManager;
-		void timeStep();
+		GameState * gameState; /* State of the whole game, containing all entities and parameters */
+		ResourceManager * resourceManager; /* Object that is responsible for updating the all resources at the end of each timestep */
+        /* Manager ensuring that all technological and "financial" requirements for the desired buildung action are fulfilled */
+		TechnologyManager * technologyManager; 
+		void timeStep(); /* Calling this function precedes to the next timestep */
 	public:
-		void startSimulation();
+        /** Function starting the Simulation
+         *
+         * After calling this function all required objects, including the whole Game State and both Managers, are created.
+         * Next the Simulation is started by entering the first timestep and proceded 
+         * by executing all necessary actions until gameState fits to the specifed goal.
+         */
+		void startSimulation(); 
 
 };
 
