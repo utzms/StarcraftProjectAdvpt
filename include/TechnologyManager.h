d69b3b60a1e46ad563504dd266e50a4763dd2915
Jonas Schmitt <josch243@gmail.com>
2013-11-14 12:55:39 +0100

    Documentation added for TechnologyManager.h

--- include/TechnologyManager.h
+++ include/TechnologyManager.h
@@ -4,14 +4,27 @@
 #include "GameState.h"
 #include "Entity.h"
 
+/** Manager class responsible for ensuring that all requirements are fulfilled before an action can be executed.
+ * 
+ * The TechnologyManager is aware of the whole tech tree of the three races 
+ * and of the amount of resources that have to be spent for creating a specific Entity.
+ * If a new Entity shall be created, request() can be called and the TechnologyManager checks if everything
+ * concerning Resources and Tech in the GameState fits the requirements for its creation.
+ * The TechnologyManager is notified whenever a change that concerns the Tech Tree occurs.
+ */
+
 class TechnologyManager
 {
 	private:
 		GameState * gameState;
-		//irgendwie muss er hier noch den tech-tree halten
+	
 	public:
-		bool Request(Entity entity);//returns true, if requested building/unit/... can be built
-		//noch eine funktion, die zurueckliefert an was die anfrage scheitert....rueckgabewert hier das problem
+        /** Function for demanding a requirement check.
+         * @param The Entity that shall be created
+         * @return true, if all requirements are fulfilled
+         * @return false, else
+         */
+		bool request(Entity entity);
 };
 
 #endif
