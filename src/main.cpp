9cd609bcc837b391163f8c204352005315a25b2c
chris <chris@chris.(none)>
2013-12-08 15:56:59 +0100

    TechList almost finished, only missing vanish and buildFrom vector Initializations Has to be discussed in group though.
    Technology had to be changed to std::vec<std::vec<Technology>> type

--- src/main.cpp
+++ src/main.cpp
@@ -18,24 +18,28 @@ int main()
 		std::cout << tec->getGasCost() << "\t";
 		std::cout << tec->getSupplyCost() << "\t";
 		std::cout << tec->getBuildTime() << "\t";
-		std::vector<std::shared_ptr<Technology>> req = tec->getRequirements();
+		std::vector<std::vector<std::shared_ptr<Technology>>> req = tec->getRequirements();
 		for(int i = 0; i < req.size(); ++i)
 		{
-			std::cout << req[i]->getName() <<","; 
+			for (int j = 0; j < req[i].size(); ++j)
+				std::cout << req[i][j]->getName() <<"/"; 
+			std::cout << ",";
 		}
 		std::cout << std::endl;
 	}
-	while (((tec=tech.findBuilding("RoboticsBay").get()) != NULL))
+	while (((tec=tech.findBuilding("CyberneticsCore").get()) != NULL))
 	{
 		std::cout << tec->getName() << "\t";
 		std::cout << tec->getMineralsCost() << "\t";
 		std::cout << tec->getGasCost() << "\t";
 		std::cout << tec->getSupplyCost() << "\t";
 		std::cout << tec->getBuildTime() << "\t";
-		std::vector<std::shared_ptr<Technology>> req = tec->getRequirements();
+		std::vector<std::vector<std::shared_ptr<Technology>>> req = tec->getRequirements();
 		for(int i = 0; i < req.size(); ++i)
 		{
-			std::cout << req[i]->getName() <<","; 
+			for (int j = 0; j < req[i].size(); ++j)
+				std::cout << req[i][j]->getName() <<"/";
+			std::cout << ",";
 		}
 		std::cout << std::endl;
 	}
