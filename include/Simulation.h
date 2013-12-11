8f070cd4598aa2310e8def7bad3b01c7e00b6aba
utzzi <utzzi@utzzi-System-Product-Name.(none)>
2013-12-11 21:36:21 +0100

    GameStateUpdate completed(not tested yet), main,simulation,ResourceManager uncommented

--- include/Simulation.h
+++ include/Simulation.h
@@ -9,9 +9,9 @@
 #include "TechnologyManager.h"
 #include "StartingConfiguration.h"
 #include "Entity.h"
-
+#include "GameStateUpdate.h"
 /** Main class designed for managing and controlling the Simulation.
- * 
+ *
  * During each timestep the Simulation decided which actions to execute and 
  * requests the TechnologyManager if the requirements for this action are fulfilled. 
  * If this applies, the actions are executed by changing the GameState object and the Entities that are part of it.
@@ -31,6 +31,7 @@ class Simulation
 		std::shared_ptr<ResourceManager> _resourceManager; /**< Object that is responsible for updating the all resources at the end of each timestep */
 		std::shared_ptr<TechnologyManager> _technologyManager; /**< Object that is responsible for updating the all technologies at the end of each timestep */
 		std::shared_ptr<StartingConfiguration> _startingConfiguration;
+        std::shared_ptr<GameStateUpdate> _gameStateUpdate;
         /**< Calling this function proceeds to the next timestep */
 
 
@@ -46,35 +47,38 @@ class Simulation
                                                std::shared_ptr<GameState> gameState,
                                                std::shared_ptr<ResourceManager> resourceManager,
 											   std::shared_ptr<TechnologyManager> technologyManager,
-											   std::shared_ptr<StartingConfiguration> startingConfiguration)
+                                               std::shared_ptr<StartingConfiguration> startingConfiguration,
+                                               std::shared_ptr<GameStateUpdate> gameStateUpdate
+                                               )
 			:_buildList(*(new BuildList(buildListFilename)))
             ,_gameState(gameState)
             ,_resourceManager(resourceManager)
             ,_technologyManager(technologyManager)
 			,_startingConfiguration(startingConfiguration)
+            ,_gameStateUpdate(gameStateUpdate)
 		{
 
-//			std::vector< std::shared_ptr<Worker>>& workerList = gameState->workerList;
+            std::vector< std::shared_ptr<Worker>>& workerList = gameState->workerList;
 
 
-//			for(int currentWorker = 0; currentWorker < _startingConfiguration->getInitialWorkerCount(); ++currentWorker)
-//			{
-//				workerList.push_back(std::shared_ptr<Worker>(new Worker("Konrad", _gameState, _technologyManager)));
-//				workerList[currentWorker]->state = Worker::State::CollectingMinerals;
-//			}
+            for(int currentWorker = 0; currentWorker < _startingConfiguration->getInitialWorkerCount(); ++currentWorker)
+            {
+                workerList.push_back(std::shared_ptr<Worker>(new Worker("Konrad")));
+                workerList[currentWorker]->state = Worker::State::CollectingMinerals;
+            }
 
-//			gameState->addMinerals(_startingConfiguration->getInitialMinerals());
-//			gameState->addGas(_startingConfiguration->getInitialVespeneGas());
+            gameState->addMinerals(_startingConfiguration->getInitialMinerals());
+            gameState->addGas(_startingConfiguration->getInitialVespeneGas());
         }
 
         void run()
         {
             int timesteps = 0;
-//            while(_gameState->getMinerals() < 200 )
-//            {
-//                timeStep();
-//                timesteps++;
-//            }
+            while(_gameState->getMinerals() < 200 )
+            {
+                timeStep();
+                timesteps++;
+            }
             std::cout << "Needed timesteps: " << timesteps << std::endl;
         }
 
@@ -90,20 +94,21 @@ class Simulation
 
         void timeStep()
         {
-//            std::vector< std::shared_ptr<Worker> >&      workerList   = _gameState->workerList;
-//            std::vector< std::shared_ptr<Building> >&    buildingList = _gameState->buildingList;
-//            std::vector< std::shared_ptr<Unit> >&        unitList     = _gameState->unitList;
+            std::vector< std::shared_ptr<Worker> >&      workerList   = _gameState->workerList;
+            std::vector< std::shared_ptr<Building> >&    buildingList = _gameState->buildingList;
+            std::vector< std::shared_ptr<Unit> >&        unitList     = _gameState->unitList;
 
-//            for (auto workerIterator : workerList)
-//            {
-//                workerIterator->timeStep();
-//            }
+            for (auto workerIterator : workerList)
+            {
+                workerIterator->timeStep();
+            }
 
-//            for (auto buildingIterator : buildingList)
-//            {
-//                buildingIterator->timeStep();
-//            }
-//            _resourceManager->timeStep();
+            for (auto buildingIterator : buildingList)
+            {
+                buildingIterator->timeStep();
+            }
+            _resourceManager->timeStep();
+            _gameStateUpdate->timeStep();
         }
 
         ~Simulation()
