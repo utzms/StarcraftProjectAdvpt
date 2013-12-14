af0f18dd00ee9b503edb3dffad9df405a19dc9f3
Christian Zoubek <ub96ekys@stud.informatik.uni-erlangen.de>
2013-12-14 13:58:03 +0100

    forgot main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 
 #include "../include/Simulation.h"
 #include "../include/TechnologyList.h"
+#include "../include/ResourceManager.h"
 #include "../include/InitTechTree.hpp"
 #include "../include/Technology.h"
 #include "../include/TechnologyManager.h"
@@ -12,17 +13,17 @@
 #include "../include/GameStateUpdate.h"
 int main()
 {
-    std::string unitPath = "../data/Protoss/units.txt";
-    std::string buildingPath = "../data/Protoss/buildings.txt";
+    std::string unitPath = "./data/Protoss/units.txt";
+    std::string buildingPath = "./data/Protoss/buildings.txt";
 
 	std::shared_ptr<GameState> gameState(new GameState());
-	std::shared_ptr<TechnologyList> technologyList(new TechnologyList(buildingPath,unitPath));
+	std::shared_ptr<TechnologyList> technologyList(new TechnologyList());
 	std::shared_ptr<ResourceManager> resourceManager(new ResourceManager(gameState, 1.f, 1.f));
 	std::shared_ptr<TechnologyManager> techManager(new TechnologyManager(gameState, technologyList));
-    std::shared_ptr<StartingConfiguration> startingConfiguration( new StartingConfiguration(std::string("../data/StartingConfiguration.txt")) );
+    std::shared_ptr<StartingConfiguration> startingConfiguration( new StartingConfiguration(std::string("./data/StartingConfiguration.txt")) );
 	std::shared_ptr<GameStateUpdate> gameStateUpdate(new GameStateUpdate(gameState));
 
-    Simulation simulation("protoss1.txt", gameState, resourceManager, techManager, startingConfiguration, gameStateUpdate);
+   	Simulation simulation("protoss1.txt", gameState, resourceManager, techManager, startingConfiguration, gameStateUpdate);
 
 	simulation.run();
 
