c5eefe52662f5e15dbf2b080d1d5e31b0e3921bf
Sabine Seufert <ug90umin@stud.informatik.uni-erlangen.de>
2013-12-14 12:57:34 +0100

    added Simulation.h

--- include/Simulation.h
+++ include/Simulation.h
@@ -34,59 +34,11 @@ class Simulation
         std::shared_ptr<GameStateUpdate> _gameStateUpdate;
         /**< Calling this function proceeds to the next timestep */
 
-        void buildBuilding(std::shared_ptr<Worker> workerForBuilding, std::string name ,int time)
-        {
+        void buildBuilding(std::shared_ptr<Worker> workerForBuilding, std::string name ,int time);
 
-            std::shared_ptr<Building> buildingToBuild = std::shared_ptr<Building>(new Building(name, time));
-            _gameState->buildingList.push_back(buildingToBuild);
-            workerForBuilding->timer = time;
-            workerForBuilding->buildingName = name;
-            workerForBuilding->state = Worker::State::Constructing;
+        void produceUnit(std::string name, int time,Building::ProductionType type);
 
-        }
-
-        void produceUnit(std::string name, int time,Building::ProductionType type)
-        {
-            std::vector< std::shared_ptr<Building> >&    buildingList = _gameState->buildingList;
-            std::shared_ptr<Building> BuildingForProduction = NULL;
-             for (auto buildingIterator : buildingList)
-             {
-                 if(buildingIterator->state == Building::State::Ready)
-                 {
-                        BuildingForProduction = buildingIterator;
-                        break;
-                 }
-             }
-
-             if(BuildingForProduction == NULL)
-             {
-                 throw std::runtime_error("GameStateUpdate::produceUnit: Wrong Order placed");
-             }
-
-            BuildingForProduction->productionType = type;
-            BuildingForProduction->productionUnitName = name;
-            BuildingForProduction->timer = time;
-            BuildingForProduction->state = Building::State::Producing;
-        }
-
-        void timeStep()
-        {
-            std::vector< std::shared_ptr<Worker> >&      workerList   = _gameState->workerList;
-            std::vector< std::shared_ptr<Building> >&    buildingList = _gameState->buildingList;
-            std::vector< std::shared_ptr<Unit> >&        unitList     = _gameState->unitList;
-
-            for (auto workerIterator : workerList)
-            {
-                workerIterator->timeStep();
-            }
-
-            for (auto buildingIterator : buildingList)
-            {
-                buildingIterator->timeStep();
-            }
-            _resourceManager->timeStep();
-            _gameStateUpdate->timeStep();
-        }
+        void timeStep();
 
 	public:
         /** Function starting the Simulation.
@@ -101,73 +53,12 @@ class Simulation
 											   std::shared_ptr<TechnologyManager> technologyManager,
                                                std::shared_ptr<StartingConfiguration> startingConfiguration,
                                                std::shared_ptr<GameStateUpdate> gameStateUpdate
-                                               )
-			:_buildList(*(new BuildList(buildListFilename)))
-            ,_gameState(gameState)
-            ,_resourceManager(resourceManager)
-            ,_technologyManager(technologyManager)
-			,_startingConfiguration(startingConfiguration)
-            ,_gameStateUpdate(gameStateUpdate)
-		{
+                                               );
 
-            std::vector< std::shared_ptr<Worker>>& workerList = gameState->workerList;
-            for(int currentWorker = 0; currentWorker < _startingConfiguration->getInitialWorkerCount(); ++currentWorker)
-            {
-                workerList.push_back(std::shared_ptr<Worker>(new Worker("Konrad")));
-                workerList[currentWorker]->state = Worker::State::CollectingMinerals;
-            }
-            buildBuilding(_gameState->workerList[0] , "Nexus", 1);
-            gameState->addMinerals(_startingConfiguration->getInitialMinerals());
-            gameState->addGas(_startingConfiguration->getInitialVespeneGas());
-        }
+        void run();
 
-        void run()
-        {
-
-            ////add here custom SIMULATION////
-            int timesteps = 0;
-            //just some time limit
-            while(timesteps < 200 )
-            {
-                //check for ready workers
-                std::vector< std::shared_ptr<Worker> >&      workerList   = _gameState->workerList;
-                for(auto workerIterator : workerList)
-                {
-                    if(workerIterator->state == Worker::State::Ready)
-                    {
-                        //send ready workers to the mineralfields
-                           workerIterator->state = Worker::State::CollectingMinerals;
-                    }
-                }
-
-                // test production of one worker
-                if( !(_gameState->buildingList.empty()) != 0 && timesteps == 120)
-                {
-                    produceUnit(std::string("Worker"), 10, Building::ProductionType::WorkerOrder );
-                }
-
-
-                timeStep();
-                timesteps++;
-            }
-            std::cout << "Produced Minerals: " << _gameState->getMinerals() << std::endl;
-        }
-
-        void startSimulation()
-        {
-            do
-            {
-				std::cout <<  _buildList.current() << std::endl;
-            }
-			while( _buildList.advance() != BuildList::State::Finished );
-            return;
-        }
-
-        ~Simulation()
-        {
-            //delete gameState;
-            //delete resourceManager;
-        }
+	void startSimulation();
+        ~Simulation();
 
 };
 
