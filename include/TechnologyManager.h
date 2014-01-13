ce7818fdce23ac6ae09168d1c8730c4884cb85cc
Christian Zoubek <ub96ekys@stud.informatik.uni-erlangen.de>
2014-01-13 10:37:38 +0100

    minifix buildlistpossible

--- include/TechnologyManager.h
+++ include/TechnologyManager.h
@@ -208,24 +208,24 @@ public:
                 return false;
             }
             fulfilled = false;
-            for(std::shared_ptr<Technology> tech : techVec)
-            {
-                if(checkIfNameIsBuilding(entityName) || tech->getSupplyCost() <= supply) 
-                {
+			for(std::shared_ptr<Technology> tech : techVec)
+			{
+				if(checkIfNameIsBuilding(entityName) || tech->getSupplyCost() <= supply) 
+				{
 					// sorry for change, this is a bad overall design bug
 					// we dont have any larvae before simulation but they are a vanishing requirement
 					if(checkTechnologyRequirements(tech))
-                    {
-                        fulfilled = true;
-                        notifyCreation(entityName);
-                        if(!entityName.compare(RacePolicy::getMainBuilding()))
-                        {
-                            supply += 10;
-                        }
-                        else if(!entityName.compare(RacePolicy::getSupplyProvider())) 
-                        {
-                            supply += 8;
-                        } else 
+					{
+						fulfilled = true;
+						notifyCreation(entityName);
+						if(!entityName.compare(RacePolicy::getMainBuilding()))
+						{
+							supply += 10;
+						}
+						else if(!entityName.compare(RacePolicy::getSupplyProvider())) 
+						{
+							supply += 8;
+						} else 
 						{
 							supply -= tech->getSupplyCost();
 							if (supply < 0)
@@ -234,142 +234,142 @@ public:
 								return false;
 							}
 						}
-                        break;
-                    }
-                }
-                if(!fulfilled) 
-                {
-			        _techList.reset();
-                    return false;
-                }
-            }
-        }
-        _techList.reset();
-        return true;
-    }
+						break;
+					}
+				}
+			}
+			if(!fulfilled) 
+			{
+				_techList.reset();
+				return false;
+			}
+		}
+		_techList.reset();
+		return true;
+	}
 
-    /** Functions for notifying state-changes in entities.
-     * @param Shared Pointer to the Entity that has changed it state
-     */
-    void notifyCreation(std::string entityName)
-    {
-        std::vector<std::shared_ptr<Technology>> techVec = /*TechnologyManager::*/findTechnology(entityName);
-        if(techVec.size() == 0)
-        {
-            throw std::invalid_argument("The requested Entity is not existent in the Tech Tree");
-        }
-        for(auto tech : techVec)
-        {
-            tech->incExistence();
-        }
-    }
+	/** Functions for notifying state-changes in entities.
+	 * @param Shared Pointer to the Entity that has changed it state
+	 */
+	void notifyCreation(std::string entityName)
+	{
+		std::vector<std::shared_ptr<Technology>> techVec = /*TechnologyManager::*/findTechnology(entityName);
+		if(techVec.size() == 0)
+		{
+			throw std::invalid_argument("The requested Entity is not existent in the Tech Tree");
+		}
+		for(auto tech : techVec)
+		{
+			tech->incExistence();
+		}
+	}
 
-    void notifyDestruction(std::string entityName)
-    {
-        std::vector<std::shared_ptr<Technology>> techVec = TechnologyManager::findTechnology(entityName);
-        if(techVec.size() == 0)
-        {
-            throw std::invalid_argument("The requested Entity is not existent in the Tech Tree");
-        }
-        for(auto tech : techVec)
-        {
-            tech->decExistence();
-        }
-    }
+	void notifyDestruction(std::string entityName)
+	{
+		std::vector<std::shared_ptr<Technology>> techVec = TechnologyManager::findTechnology(entityName);
+		if(techVec.size() == 0)
+		{
+			throw std::invalid_argument("The requested Entity is not existent in the Tech Tree");
+		}
+		for(auto tech : techVec)
+		{
+			tech->decExistence();
+		}
+	}
 
-    // Functions for deciding if a name (e.g. from buildList)
-    // is a building or a unit
-    bool checkIfNameIsBuilding(std::string entityName)
-    {
-        bool result = false;
+	// Functions for deciding if a name (e.g. from buildList)
+	// is a building or a unit
+	bool checkIfNameIsBuilding(std::string entityName)
+	{
+		bool result = false;
 
-        auto technology = _techList.findBuilding(entityName);
-        if(technology != nullptr)
-        {
-            result = true;
-        }
+		auto technology = _techList.findBuilding(entityName);
+		if(technology != nullptr)
+		{
+			result = true;
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    bool checkIfNameIsUnit(std::string entityName)
-    {
-        bool result = false;
+	bool checkIfNameIsUnit(std::string entityName)
+	{
+		bool result = false;
 
-        auto technology = _techList.findUnit(entityName);
-        if(technology != nullptr)
-        {
-            result = true;
-        }
+		auto technology = _techList.findUnit(entityName);
+		if(technology != nullptr)
+		{
+			result = true;
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    Costs getEntityCosts(std::string entityName)
-    {
-        std::shared_ptr<Technology> technology;
+	Costs getEntityCosts(std::string entityName)
+	{
+		std::shared_ptr<Technology> technology;
 
-        if (checkIfNameIsBuilding(entityName))
-        {
-            technology = _techList.findBuilding(entityName);
-        }
-        else if (checkIfNameIsUnit(entityName))
-        {
-            technology = _techList.findUnit(entityName);
-        }
-        else
-        {
-            throw std::invalid_argument("TechnologyManager::getEntityCosts() entity does not exist in tech list.");
-        }
+		if (checkIfNameIsBuilding(entityName))
+		{
+			technology = _techList.findBuilding(entityName);
+		}
+		else if (checkIfNameIsUnit(entityName))
+		{
+			technology = _techList.findUnit(entityName);
+		}
+		else
+		{
+			throw std::invalid_argument("TechnologyManager::getEntityCosts() entity does not exist in tech list.");
+		}
 
-        return technology->TechCosts;
-    }
+		return technology->TechCosts;
+	}
 
-    std::vector<std::string> getBuildingsForUnitProduction(std::string unitName)
-    {
-        std::shared_ptr<Technology> technology;
+	std::vector<std::string> getBuildingsForUnitProduction(std::string unitName)
+	{
+		std::shared_ptr<Technology> technology;
 
-        if (checkIfNameIsUnit(unitName))
-        {
-            technology = _techList.findUnit(unitName);
-        }
-        else
-        {
-            throw std::invalid_argument("TechnologyManager::getBuildingForUnitProduction() entity is not a unit.");
-        }
+		if (checkIfNameIsUnit(unitName))
+		{
+			technology = _techList.findUnit(unitName);
+		}
+		else
+		{
+			throw std::invalid_argument("TechnologyManager::getBuildingForUnitProduction() entity is not a unit.");
+		}
 
-        auto requirements = technology->getRequirements();
-        std::vector<std::string> buildings;
+		auto requirements = technology->getRequirements();
+		std::vector<std::string> buildings;
 
-        for (auto outerIterator : requirements)
-        {
-            for (auto innerIterator : outerIterator)
-            {
-                if (innerIterator.second == RequirementType::ForProduction)
-                {
-                    buildings.push_back(innerIterator.first->getName());
-                }
-            }
-        }
+		for (auto outerIterator : requirements)
+		{
+			for (auto innerIterator : outerIterator)
+			{
+				if (innerIterator.second == RequirementType::ForProduction)
+				{
+					buildings.push_back(innerIterator.first->getName());
+				}
+			}
+		}
 
-        return buildings;
-    }
+		return buildings;
+	}
 
-    bool buildingExists(std::string buildingName)
-    {
-        std::shared_ptr<Technology> technology;
+	bool buildingExists(std::string buildingName)
+	{
+		std::shared_ptr<Technology> technology;
 
-        if (checkIfNameIsBuilding(buildingName))
-        {
-            technology = _techList.findBuilding(buildingName);
-        }
-        else
-        {
-            throw std::invalid_argument("TechnologyManager::getBuildingForUnitProduction() entity is not a unit.");
-        }
+		if (checkIfNameIsBuilding(buildingName))
+		{
+			technology = _techList.findBuilding(buildingName);
+		}
+		else
+		{
+			throw std::invalid_argument("TechnologyManager::getBuildingForUnitProduction() entity is not a unit.");
+		}
 
-        return technology->exists();
-    }
+		return technology->exists();
+	}
 };
 
 #endif
