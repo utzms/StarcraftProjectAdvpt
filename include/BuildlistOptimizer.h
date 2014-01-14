d160911236b05460ccdd55b728fe62ec1d000669
josch <josch243@gmail.com>
2014-01-14 13:19:04 +0100

    BuildlistOptimizer.h: Continued work, finished so far until it comes to my mind that I forgot something

--- include/BuildlistOptimizer.h
+++ include/BuildlistOptimizer.h
@@ -6,6 +6,7 @@
 #include <map>
 #include <memory>
 #include <vector>
+#include <utility>
 #include "Simulation.h"
 #include "BuildList.h"
 
@@ -13,23 +14,45 @@
 using std::string;
 using std::vector;
 using std::shared_ptr;
+using std::map;
 
 template <class FitnessPolicy>
 class BuildlistOptimizer
 {
 
 private:
-        std::map<int, BuildList> mPopulation;
+        map<int, BuildList> mPopulation;
         std::hash<string> mHashGen;
         int mTimeLimit;
-        void initialize(int initPopSize);
+
+        void crossover(float reproductionRate);
         void mutate(float mutationRate);
         void select(float selectionRate);
 
 
 public:
         BuildlistOptimizer(int timeLimit);
-        shared_ptr< vector<string> > optimize(int initPopSize, float mutationRate, float selectionRate);
+
+        /*initializes the population with random individuals until the population size reaches initPopSize*/
+        void initialize(int initPopSize);
+
+        /* clears the whole population by terminating (sic!) all individuals */
+        void clear(void);
+
+        /* optimizes a buildList by mutating, crossing over and selecting the fittest individuals */
+        void optimize(float reproductionRate, float mutationRate, float selectionRate);
+
+        /* combined use of initialize and optimize */
+        void initializeAndOptimize(int initPopSize, float reproductionRate, float mutationRate, float selectionRate);
+
+        /* adds a specific individual to the population */
+        void addIndividual(BuildList buildlist);
+
+        /* get the group of the fittest individuals with size number */
+        void getFittestGroup(int number, vector< pair<int,BuildList> >& res);
+
+        /* get the overall fittest individual */
+        shared_ptr< pair<int,BuildList> > getFittestIndividual(void);
 
 };
 
