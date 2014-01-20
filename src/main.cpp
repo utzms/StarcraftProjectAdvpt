659414730d357bd86a2622b74f6009eb7a6bf21e
Christian Zoubek <ub96ekys@stud.informatik.uni-erlangen.de>
2014-01-20 11:45:59 +0100

    Zerg apparently not working anymore

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,66 @@
 
-
 #include "RacePolicy.h"
 #include "FitnessPolicy.h"
 #include "BuildListOptimizer.cpp"
 
 #include <stdexcept>
 
+int oldCall(int argc, char **argv)
+{
+	if(argc < 2)
+	{
+		std::cerr << "Too few arguments" << std::endl;
+	}
+	std::string argument(argv[1]);
+	
+	try
+	{
+		Simulation<Protoss> simulation(argument);
+		simulation.run();
+		return 0;
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << "Protoss Error Message: " << e.what() << std::endl;
+	}
+
+	try
+	{
+		Simulation<Terran> simulation(argument);
+		simulation.run();
+		return 0;
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << "Terran Error Message: " << e.what() << std::endl;
+	}
+
+	try
+	{
+		Simulation<Zerg> simulation(argument);
+		simulation.run();
+		return 0;
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << "Zerg Error Message: " << e.what() << std::endl;
+	}
+
+	std::cerr << "BuildList invalid. Cannot be built by any race." << std::endl;
+	return 1;
+}
+
+
+
 int main(int argc, char *argv[])
 {
+	oldCall(argc, argv);
 	size_t a=25;
 	BuildListOptimizer<Protoss,Debug> opt(100,a);
 	opt.initialize("Zealot",10,600,1000);
 	opt.optimize("Zealot",10,600,5,5,5,50);
 	//std::cout << opt.getFittestGroup(10) << std::endl;
+
 	return  0;
 }
 
